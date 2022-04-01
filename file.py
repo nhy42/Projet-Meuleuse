98b70a5fdd14d85b47006b94176881bbc00e76db
nhy42 <26034461+nhy42@users.noreply.github.com>
2022-04-02 00:09:55 +0200

    trop mim's du JSON
    "UwU"
    + un joli fond UwU

---
+++ file.py
@@ -0,0 +1,17 @@
+import json
+
+
+def readJSONFile(path):
+    jsonObject = {}
+    with open(path, "r") as f:
+        jsonObject = json.load(f)
+    return jsonObject
+
+
+def saveIntoJSON(path, dictionary):
+    dataToWrite = json.dumps(dictionary)
+    with open(path, "w") as f:
+        f.write(dataToWrite)
+    return 0
+
+
